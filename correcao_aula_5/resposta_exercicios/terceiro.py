72c5713f5e390593c8174599d0bc60359986b756
Raphael Paiva <raphaelj.dev@gmail.com>
2025-07-25 17:33:42 -0300

    Update terceiro.py

--- correcao_aula_5/resposta_exercicios/terceiro.py
+++ correcao_aula_5/resposta_exercicios/terceiro.py
@@ -12,3 +12,20 @@ if quantidade > 0:
     print("Média =", media)
 else:
     print("Nenhuma nota válida foi digitada.")
+
+
+# soma = 0
+# quantidade = 0
+
+# while True:
+#     nota = float(input("Digite uma nota (-1 para parar): "))
+#     if nota == -1:
+#         break
+#     soma += nota
+#     quantidade += 1
+
+# if quantidade > 0:
+#     media = soma / quantidade
+#     print("Média =", media)
+# else:
+#     print("Nenhuma nota válida foi digitada.")
